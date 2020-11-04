8da253304cd9857f52adb96cea57fd4c9caf0196
Ahmed Helmy <helmyahmed248@gmail>
2020-11-04 16:05:20 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
+import gql from "graphql-tag"; 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      books: []
+    };
+  },
+  apollo: {
+    books: gql`
+      {
+        books {
+          id
+          name
+        }
+      }
+    `
   }
 };
 </script>
